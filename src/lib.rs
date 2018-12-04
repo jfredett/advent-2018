c055db815937c7c19b3047963d151e2c268d4d07
Joe Fredette <jfredett@gmail.com>
2018-12-03 19:36:41 -0500

    implement day3 generator with nom
    Overkill much?

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,16 @@
 
 extern crate aoc_runner;
 
+#[macro_use]
+extern crate nom;
+
 pub mod day1;
 pub mod day2;
+pub mod day3;
+
 
 #[macro_use]
 extern crate aoc_runner_derive;
+
 
 aoc_lib! {year = 2018}
